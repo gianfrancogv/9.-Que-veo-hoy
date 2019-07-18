9527449fb24817e113765f036115bcbc421efa3b
Gian Franco <gianfrancogv@gmail.com>
2019-07-17 21:29:33 -0300

    Paso 2.2 agregando generos

--- scripts/script.sql
+++ scripts/script.sql
@@ -2,15 +2,23 @@ CREATE DATABASE queveohoy;
 
 USE queveohoy;
 
+CREATE TABLE genero (
+    id INT NOT NULL AUTO_INCREMENT,
+    NOMBRE VARCHAR (30),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE pelicula (
 	id INT NOT NULL AUTO_INCREMENT,
 	titulo VARCHAR (100),
-    anio INT(5),
-	duracion INT(5),
+	duracion INT,
 	director VARCHAR (400),
+	anio INT,
 	fecha_lanzamiento DATE,
-	puntuacion INT(2),
+	puntuacion INT,
 	poster VARCHAR (300),
 	trama VARCHAR (700),
-	PRIMARY KEY (id)
+	genero_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (genero_id) REFERENCES genero(id)
 );
